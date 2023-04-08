56e38b4cd1267ba2777b6e535f6496a19e9fff96
Bravo <huynh.tinh@legato.co>
2023-04-08 15:56:35 +0700

    riverx customer template

--- bricks/ddd/__brick__/{{name}}/application/cubit/{{name.snakeCase()}}_state.dart
+++ bricks/ddd/__brick__/{{name}}/application/cubit/{{name.snakeCase()}}_state.dart
@@ -1,36 +1,7 @@
 part of '{{name.snakeCase()}}_cubit.dart';
 
-@generate
-enum Ct{{name.pascalCase()}}Status {
-  initial,
-  loading,
-  error,
-  loaded;
-}
-
 
 @freezed
 class Ct{{name.pascalCase()}}State with _$Ct{{name.pascalCase()}}State {
-  const Ct{{name.pascalCase()}}State._();
-
-  const factory Ct{{name.pascalCase()}}State({
-    @Default(Ct{{name.pascalCase()}}Status.initial) Ct{{name.pascalCase()}}Status status,
-    ICt{{name.pascalCase()}}? data,
-    ApiError? error,
-  }) = _Ct{{name.pascalCase()}}State;
-
-}
-
-extension Ct{{name.pascalCase()}}StateX on Ct{{name.pascalCase()}}State {
-  Ct{{name.pascalCase()}}State get loading => copyWith(status: Ct{{name.pascalCase()}}Status.loading);
-
-  Ct{{name.pascalCase()}}State onError(ApiError error) => copyWith(
-        status: Ct{{name.pascalCase()}}Status.error,
-        error: error,
-      );
-
-  Ct{{name.pascalCase()}}State onLoaded(ICt{{name.pascalCase()}} data) => copyWith(
-        status: Ct{{name.pascalCase()}}Status.loaded,
-        data: data,
-      );
+  const factory Ct{{name.pascalCase()}}State.initial() = _Initial;
 }
