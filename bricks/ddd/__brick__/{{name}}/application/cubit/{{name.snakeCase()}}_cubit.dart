77bd4572e886c941b3a3b0eb728d7ed71d010e4c
Bravo <huynh.tinh@legato.co>
2023-04-08 15:59:32 +0700

    riverx customer template

--- bricks/ddd/__brick__/{{name}}/application/cubit/{{name.snakeCase()}}_cubit.dart
+++ bricks/ddd/__brick__/{{name}}/application/cubit/{{name.snakeCase()}}_cubit.dart
@@ -13,14 +13,6 @@ part '{{name.snakeCase()}}_cubit.freezed.dart';
 @injectable
 class Ct{{name.pascalCase()}}Cubit extends Cubit<Ct{{name.pascalCase()}}State> with CancelableBaseBloc {
   final ICt{{name.pascalCase()}}Repository _repository;
-  Ct{{name.pascalCase()}}Cubit(this._repository) : super(const Ct{{name.pascalCase()}}State());
-
-  get() async {
-    emit(state.loading);
-    final response = await _repository.getById(1, token: cancelToken);
-    response.fold(
-      (l) => emit(state.onError(l)),
-      (r) => emit(state.onLoaded(r)),
-    );
-  }
+  Ct{{name.pascalCase()}}Cubit(this._repository) : super(const Ct{{name.pascalCase()}}State.Initial());
+ 
 }
